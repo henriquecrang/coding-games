210491a8c7933a9c9d9a391649dd693e1427f190
henriquecrang <github@henriqueandrade.com.br>
2022-12-18 09:06:45 -0300

    commenting challnge's description

--- 20221218.rb
+++ 20221218.rb
@@ -1,29 +1,28 @@
-The program:
-You are required to write a program that finds the number of vowels in each word of a sentence.
-
-- Words are separated by a single space.
-- The sentence may consist of letters, numbers and/or special characters. Specifically, it may contain any character in the range of characters having an ASCII value of 33 to 126 inclusive.
-INPUT:
-A single line of text consisting of at least one word.
-
-OUTPUT:
-On a single line, output the number of vowels in each word of the provided sentence. Numbers are separated by a single space.
-
-CONSTRAINTS:
-1 ≤ Length of the sentence < 1024
-
-List of vowels your program needs to consider:
-A
-E
-I
-O
-U
-a
-e
-i
-o
-u
-
+#The program:
+#You are required to write a program that finds the number of vowels in each word of a sentence.
+#
+#- Words are separated by a single space.
+#- The sentence may consist of letters, numbers and/or special characters. Specifically, it may contain any character in the range of characters having an ASCII value of 33 to 126 inclusive.
+#INPUT:
+#A single line of text consisting of at least one word.
+#
+#OUTPUT:
+#On a single line, output the number of vowels in each word of the provided sentence. Numbers are separated by a single space.
+#
+#CONSTRAINTS:
+#1 ≤ Length of the sentence < 1024
+#
+#List of vowels your program needs to consider:
+#A
+#E
+#I
+#O
+#U
+#a
+#e
+#i
+#o
+#u
 
 sentence = gets.chomp.split
 
