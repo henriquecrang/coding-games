9840a50bf1a2ce14ab4f75e6597a44493136bde7
henriquecrang <github@henriqueandrade.com.br>
2022-12-17 21:11:20 -0300

    TIL: in Ruby you can calculate power using the operator **

---
+++ 20221217.rb
@@ -0,0 +1,23 @@
+#The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+# Input
+# 1
+# Expected output
+# 3
+# Input
+# 2
+# Expected output
+# 8
+# Input
+# 5
+# Expected output
+# 35
+# Input
+# 16
+# Expected output
+# 288
+# Input
+# 18
+# Expected output
+# 360
+a = gets.to_i
+puts a**2+a+a
