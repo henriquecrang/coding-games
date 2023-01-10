4dadce182c27760d0435b5c4f30241e0d3057ef0
Henrique de Andrade <henriquedeandrade@gmail.com>
2023-01-10 09:30:43 -0300

    Commenting the challenge definition

--- clash_of_codes/20230109.rb
+++ clash_of_codes/20230109.rb
@@ -1,35 +1,35 @@
-The program:
-Your program must read the string given on the standard input and print to the standard output the same string converted into 1337 speak.
-
-To convert text into 1337 speak, one must replace (whether upper or lower case):
-
-    'O' with '0'.
-    'L' with '1'.
-    'Z' with '2'.
-    'E' with '3'.
-    'A' with '4'.
-    'S' with '5'.
-    'G' with '6'.
-    'T' with '7'.
-    'B' with '8'.
-    'Q' with '9'.
-
-
-INPUT:
-A string S.
-
-OUTPUT:
-S converted to 1337 speak.
-
-CONSTRAINTS:
-S contains at least 1 character.
-
-EXAMPLE:
-Input
-Hello World
-Output
-H3110 W0r1d
-
+#The program:
+#Your program must read the string given on the standard input and print to the standard output the same string converted into 1337 speak.
+#
+#To convert text into 1337 speak, one must replace (whether upper or lower case):
+#
+#    'O' with '0'.
+#    'L' with '1'.
+#    'Z' with '2'.
+#    'E' with '3'.
+#    'A' with '4'.
+#    'S' with '5'.
+#    'G' with '6'.
+#    'T' with '7'.
+#    'B' with '8'.
+#    'Q' with '9'.
+##
+#
+#INPUT:
+#A string S.
+#
+#OUTPUT:
+#S converted to 1337 speak.
+#
+#CONSTRAINTS:
+#S contains at least 1 character.
+#
+#EXAMPLE:
+#Input
+#Hello World
+#Output
+#H3110 W0r1d
+#
 #
 def leet(a)
     x = a.upcase
