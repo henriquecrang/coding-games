740b449f3c11137024f7a5ae87fe9cf7d2e4e577
henriquecrang <github@henriqueandrade.com.br>
2023-01-13 14:04:26 -0300

    TIL: when using ? after a function you need to add an extra ? to capture the boolean result and call other methods

---
+++ clash_of_codes/20230113.rb
@@ -0,0 +1,10 @@
+#
+#The statement is simple: Given a positive integer, you will have to apply a set of rules to it to form a sequence.
+#
+#If the number is even, divide it by 2.
+#If the number is odd, multiply it by 3 and add 1 to it.
+
+n = gets.to_i
+
+puts n.even? ? (n*2) : (n * 3 + 1)
+
