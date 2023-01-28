49783af247895c0c5b53dff0265c302571025557
henriquecrang <github@henriqueandrade.com.br>
2023-01-28 09:01:45 -0300

    TIL: in Ruby the power function is represeted by the ** operator

---
+++ clash_of_codes/20230128.rb
@@ -0,0 +1,20 @@
+# Golf
+#Goal
+#You have an integer X. Your objective is to find the absolute difference between the value X to the power 2 and (X+1) to the power 2.
+#Input
+#An integer X.
+#Output
+#The absolute difference between X to the power 2 and (X+1) to the power 2.
+#Constraints
+#1 < X < 2500000000
+#Example
+#Input
+#
+#2
+#
+#Output
+#
+#5
+##
+x=gets.to_i
+puts (x*x-(x+1)**2).abs
