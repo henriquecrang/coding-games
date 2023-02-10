db660262e34e8ff9ce386d9407e6e2544751a38d
henriquecrang <github@henriqueandrade.com.br>
2023-02-10 14:17:06 -0300

    TIL: Strings have a reverse() method

---
+++ clash_of_codes/20230210.rb
@@ -0,0 +1,10 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a = gets.chomp
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+(a == a.reverse)? puts("wins"): puts("loses")
+
