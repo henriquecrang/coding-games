9f20a83e142e1f63e19726bbbd3969fcc2667aca
henriquecrang <github@henriqueandrade.com.br>
2023-05-01 10:11:22 -0300

    TIL: Strings have a count() method that gets a char as param and return

---
+++ clash_of_codes/20230501.rb
@@ -0,0 +1,26 @@
+# Goal
+The elevator floor display had a problem and displayed its log up/down of the day with brackets ( for go up one floor and ) for go down one floor. The elevator starts on floor 0 and there is no limit to the number of floors in both up (positive) and down (negative) directions
+Input
+A string log from the elevator display log
+Output
+An integer floor telling the floor where we are
+Constraints
+4 ≤ length of log ≤ 5600
+Example
+Input
+
+(())
+
+Output
+
+0
+
+
+######
+
+
+log = gets.chomp
+
+puts log.count('(') - log.count(')')
+
+
