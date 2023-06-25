2087fc8355cdf955bfcbfcaa37ea0201cd1cb30a
henriquecrang <github@henriqueandrade.com.br>
2023-06-24 23:55:51 -0300

    Adicionando comentários

--- clash_of_codes/20230614.rb
+++ clash_of_codes/20230614.rb
@@ -1,43 +1,44 @@
 # DNA strands are composed of four nucleotide bases: adenine (A), cytosine (C), guanine (G), and thymine (T)
+#
 
-GC-content of DNA describes the ratio of guanine & cytosine nucleotide bases compared to other nucleotides in the dna strand.
+#GC-content of DNA describes the ratio of guanine & cytosine nucleotide bases compared to other nucleotides in the dna strand.
 
-GC% is significant in various biological applications. For example, it can be used to infer the stability and melting temperature of DNA.
+#GC% is significant in various biological applications. For example, it can be used to infer the stability and melting temperature of DNA.
 
-Here, you have to calculate the GC% for each input sequence representing a DNA strand and print out the percentage, rounded to one decimal place.
+#Here, you have to calculate the GC% for each input sequence representing a DNA strand and print out the percentage, rounded to one decimal place.
 
-You can assume that the input strings are only made up of valid symbols.
-Input
-Line 1: An integer N for the number of sequences
-Next N lines: A string sequence for a DNA strand
-Output
-N lines: The GC percentage of each string rounded to one decimal place
-Constraints
-1 ≤ N < 100
-1 < length of sequence < 10000
-Example
-Input
+#You can assume that the input strings are only made up of valid symbols.
+#Input
+#Line 1: An integer N for the number of sequences
+#Next N lines: A string sequence for a DNA strand
+#Output
+#N lines: The GC percentage of each string rounded to one decimal place
+#Constraints
+#1 ≤ N < 100
+#1 < length of sequence < 10000
+#Example
+#Input
 
-8
-A
-G
-AAAA
-TTTT
-TATA
-AAAG
-GGGA
-TATATATAG
+#8
+#A
+#G
+#AAAA
+#TTTT
+#TATA
+#AAAG
+#GGGA
+#TATATATAG
 
-Output
+#Output
 
-0.0
-100.0
-0.0
-0.0
-0.0
-25.0
-75.0
-11.1
+#0.0
+#100.0
+#0.0
+#0.0
+#0.0
+#25.0
+#75.0
+#11.1
 
 
 #####
